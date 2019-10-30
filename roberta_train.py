8db9297075e2f5373fa766fa2e2ce7fffc9e56af
Justin Cho <doij790790@gmail.com>
2019-10-29 22:36:40 -0700

    added base experiment configurations

--- roberta_train.py
+++ roberta_train.py
@@ -7,6 +7,7 @@ from pprint import pformat
 from argparse import ArgumentParser
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score
+from pathlib import Path 
 
 import torch 
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
@@ -19,42 +20,64 @@ from ignite.contrib.metrics import GpuInfo
 from ignite.contrib.handlers import ProgressBar, PiecewiseLinear
 from ignite.contrib.handlers.tensorboard_logger import TensorboardLogger, OutputHandler, OptimizerParamsHandler
 
-from utils import  get_data, YESAND_DATAPATH, build_roberta_input, ROBERTA_MAX_LEN
+from utils import  get_data, build_roberta_input, ROBERTA_MAX_LEN
 
 logger = logging.getLogger(__file__)
 
-# recommended settings: batch size = 32, max length = 128 
+def extract_base_train_data(data): 
+    # from the entire training set, get only the Spontaneanation yes-ands and cornell non-yes-ands of equal length 
 
-def get_data_loaders(args, tokenizer):
+    new_data = {
+        'yes-and': {
+            'spont': data['yes-and']['spont']
+        }, 
+        'non-yes-and': {
+            'cornell': data['non-yes-and']['cornell'][:len(data['yes-and']['spont'])]
+        }
+    }
 
-    data = get_data(args.data_path)
-    all_samples = build_roberta_input(data, args.data_path, tokenizer)
-    random.shuffle(all_samples)
-    train_samples, validation_samples = train_test_split(all_samples, random_state=args.random_seed, test_size=args.test_size)
+    assert len(new_data['yes-and']['spont']) == len(new_data['non-yes-and']['cornell'])
 
-    train_inputs, train_labels, train_masks, train_token_types = [s['input_ids'] for s in train_samples], [s['label'] for s in train_samples], [s['attention_mask'] for s in train_samples], [s['token_type_ids'] for s in train_samples] 
-    validation_inputs, validation_labels, validation_masks, validation_token_types = [s['input_ids'] for s in validation_samples], [s['label'] for s in validation_samples], [s['attention_mask'] for s in validation_samples], [s['token_type_ids'] for s in validation_samples] 
+    return new_data
+
+def get_data_loaders(args: object, data: dict, datapath: str, tokenizer: object):
+
+    if args.base and 'train' in datapath: 
+        # adjust the data object to contain only the base dataset (spont yesands and matching cornell non-yesands)
+        logger.info("Extracting base dataset for base experiment...")
+        data = extract_base_train_data(data) 
+        datapath = f"{str(Path(datapath).parent)}/base_{Path(datapath).name}"
+
+    samples = build_roberta_input(data, datapath, tokenizer)
+
+    if args.test and 'train' in datapath: 
+        samples = samples[:args.batch_size]
+
+    # no need to shuffle because the random sampler with sample randomly for the train set 
+    inputs, labels, masks, token_types = [s['input_ids'] for s in samples], [s['label'] for s in samples], [s['attention_mask'] for s in samples], [s['token_type_ids'] for s in samples] 
 
     # wrap the data as tensors 
-    train_inputs, train_labels, train_masks, train_token_types = [torch.tensor(x) for x in [train_inputs, train_labels, train_masks, train_token_types]]
-    validation_inputs, validation_labels, validation_masks, validation_token_types = [torch.tensor(x) for x in [validation_inputs, validation_labels, validation_masks, validation_token_types]]
+    inputs, labels, masks, token_types = [torch.tensor(x) for x in [inputs, labels, masks, token_types]]
 
     # group as tensor datasets
-    train_data = TensorDataset(train_inputs, train_masks, train_token_types, train_labels)
-    validation_data = TensorDataset(validation_inputs, validation_masks, validation_token_types, validation_labels)
+    tensors = TensorDataset(inputs, masks, token_types, labels)
 
+    assert 'train' in datapath or 'valid' in datapath, "Either 'train' or 'valid' should be in datapath."
     # build dataloaders
-    train_sampler = RandomSampler(train_data) 
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size = args.batch_size)
+    if 'train' in datapath:
+        sampler = RandomSampler(tensors) 
+    elif 'valid' in datapath: 
+        sampler = SequentialSampler(tensors)
 
-    validation_sampler = SequentialSampler(validation_data)
-    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size = args.batch_size)
+    dataloader = DataLoader(tensors, sampler=sampler, batch_size = args.batch_size)
 
-    return train_dataloader, validation_dataloader, train_sampler, validation_sampler
+    return dataloader, sampler 
 
 def train(): 
     parser = ArgumentParser()
-    parser.add_argument("--data_path", type=str, default=YESAND_DATAPATH, help="Set data path")    
+    parser.add_argument("--train_path", type=str, default='data/yesand_train.json', help="Set data path")    
+    parser.add_argument("--valid_path", type=str, default='data/yesand_valid.json', help="Set data path")    
+
     parser.add_argument("--correct_bias", type=bool, default=False, help="Set to true to correct bias for Adam optimizer")
     parser.add_argument("--lr", type=float, default=5e-6, help="Set learning rate")
     parser.add_argument("--n_epochs", type=int, default=5, help="Set number of epochs")
@@ -62,10 +85,11 @@ def train():
     parser.add_argument("--num_total_steps", type=float, default=10000, help="Set number of total steps")
     parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
     parser.add_argument("--max_grad_norm", type=float, default=1.0, help="Set maximum gradient normalization.")
-    parser.add_argument("--test_size", type=float, default=0.1, help="Set proportion of validation size split")
     parser.add_argument("--pretrained_path", type=str, default='roberta-base', help="Choose which pretrained Roberta to use (roberta-base, roberta-large, roberta-large-mnli)")    
     parser.add_argument("--batch_size", type=int, default=32, help="Provide the batch size")    
     parser.add_argument("--random_seed", type=int, default=42, help="Set the random seed")
+    parser.add_argument("--test", action='store_true', help="If true, run with small dataset for testing code")
+    parser.add_argument("--base", action='store_true', help="If true, run with base experiment configuration (training with spont only) for comparison")
 
     args = parser.parse_args() 
 
@@ -103,11 +127,25 @@ def train():
     scheduler = WarmupLinearSchedule(optimizer, warmup_steps=args.num_warmup_steps, t_total=args.num_total_steps) 
 
     logger.info("Prepare datasets")
-    train_loader, valid_loader, train_sampler, valid_sampler = get_data_loaders(args, tokenizer)
+    logger.info("Loading train set...")
+
+    train_data = get_data(args.train_path)
+    valid_data = get_data(args.valid_path)
+
+    cornell_valid_data = {k: {'cornell': valid_data[k]['cornell']} for k in valid_data.keys()}
+    spont_valid_data = {k: {'spont': valid_data[k]['spont']} for k in valid_data.keys()}
+
+    train_loader, train_sampler = get_data_loaders(args, train_data, args.train_path, tokenizer)
+    logger.info("Loading validation set...")
+    valid_p = Path(args.valid_path)
+    cornell_valid_loader, cornell_valid_sampler = get_data_loaders(args, cornell_valid_data, f"{str(valid_p.parent)}/cornell_{valid_p.name}",  tokenizer)
+    spont_valid_loader, spont_valid_sampler = get_data_loaders(args, spont_valid_data, f"{str(valid_p.parent)}/spont_{valid_p.name}", tokenizer)
+
 
     # Training function and trainer 
     def update(engine, batch): 
         model.train() 
+
         batch = tuple(input_tensor.to(args.device) for input_tensor in batch)
         b_input_ids, b_input_mask, b_input_segment, b_labels = batch
 
@@ -141,9 +179,13 @@ def train():
             label_ids = b_labels
 
         return logits, label_ids, loss.item()
-    evaluator = Engine(inference)
+    cornell_evaluator = Engine(inference)
+    spont_evaluator = Engine(inference)
 
-    trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda _: evaluator.run(valid_loader))
+
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda _: cornell_evaluator.run(cornell_valid_loader))
+    trainer.add_event_handler(Events.EPOCH_COMPLETED, lambda _: spont_evaluator.run(spont_valid_loader))
+
 
     RunningAverage(output_transform=lambda x: x[0]).attach(trainer, "loss") 
     RunningAverage(Accuracy(output_transform=lambda x: (x[1], x[2]))).attach(trainer, "accuracy")
@@ -157,21 +199,27 @@ def train():
     metrics = {"recall": recall, "precision": precision, "f1": F1, "accuracy": accuracy, "loss": Average(output_transform=lambda x: x[2])}
 
     for name, metric in metrics.items(): 
-        metric.attach(evaluator, name) 
+        metric.attach(cornell_evaluator, name) 
+        metric.attach(spont_evaluator, name) 
+
 
     pbar = ProgressBar(persist=True)
     pbar.attach(trainer, metric_names=['loss', 'accuracy'])
     pbar.attach(trainer, metric_names=['gpu:0 mem(%)', 'gpu:0 util(%)'])
     
-    evaluator.add_event_handler(Events.COMPLETED, lambda _: pbar.log_message("Validation %s" % pformat(evaluator.state.metrics)))
+    cornell_evaluator.add_event_handler(Events.COMPLETED, lambda _: pbar.log_message("Cornell validation metrics:\n %s" % pformat(cornell_evaluator.state.metrics)))
+    spont_evaluator.add_event_handler(Events.COMPLETED, lambda _: pbar.log_message("Spont validation metrics:\n %s" % pformat(spont_evaluator.state.metrics)))
+
 
     tb_logger = TensorboardLogger(log_dir=None)
     tb_logger.attach(trainer, log_handler=OutputHandler(tag="training", metric_names=["loss"]), event_name=Events.ITERATION_COMPLETED)
     tb_logger.attach(trainer, log_handler=OptimizerParamsHandler(optimizer), event_name=Events.ITERATION_STARTED)
-    tb_logger.attach(evaluator, log_handler=OutputHandler(tag="validation", metric_names=list(metrics.keys()), another_engine=trainer), event_name=Events.EPOCH_COMPLETED)
+    tb_logger.attach(cornell_evaluator, log_handler=OutputHandler(tag="valid", metric_names=list(metrics.keys()), another_engine=trainer), event_name=Events.EPOCH_COMPLETED)
+    tb_logger.attach(spont_evaluator, log_handler=OutputHandler(tag="valid", metric_names=list(metrics.keys()), another_engine=trainer), event_name=Events.EPOCH_COMPLETED)
+
 
     # tb_logger.writer.log_dir -> tb_logger.writer.logdir (this is the correct attribute name as seen in: https://tensorboardx.readthedocs.io/en/latest/_modules/tensorboardX/writer.html#SummaryWriter)
-    checkpoint_handler = ModelCheckpoint(tb_logger.writer.logdir, 'checkpoint', save_interval=1, n_saved=3)
+    checkpoint_handler = ModelCheckpoint(tb_logger.writer.logdir, 'checkpoint', save_interval=1, n_saved=5)
     trainer.add_event_handler(Events.EPOCH_COMPLETED, checkpoint_handler, {'mymodel': getattr(model, 'module', model)})  # "getattr" take care of distributed encapsulation
 
     torch.save(args, tb_logger.writer.logdir + '/model_training_args.bin')
